f5bcb177bc582696d238fe8bc05a13bb41f4c767
Dale Bingham <dalebingham@Dales-Main-Macbook.local>
2020-01-15 17:53:56 -0500

    version 0.10.9 updates

--- src/Program.cs
+++ src/Program.cs
@@ -27,7 +27,7 @@ namespace openrmf_msg_checklist
             // add the options for the server, reconnecting, and the handler events
             Options opts = ConnectionFactory.GetDefaultOptions();
             opts.MaxReconnect = -1;
-            opts.ReconnectWait = 1000;
+            opts.ReconnectWait = 2000;
             opts.Name = "openrmf-msg-checklist";
             opts.Url = Environment.GetEnvironmentVariable("NATSSERVERURL");
             opts.AsyncErrorEventHandler += (sender, events) =>
